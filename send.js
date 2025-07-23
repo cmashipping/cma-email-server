5c87817e35ff0a7527395b0c43e85f22d42559d1
Afnan Amry <no-reply@cmasealand.com>
2025-07-22 20:45:35 -0700

    Initial commit for email server

---
+++ send.js
@@ -0,0 +1,34 @@
+
+const nodemailer = require("nodemailer");
+
+module.exports = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).send("Only POST requests allowed");
+  }
+
+  const { to, subject, html } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtpout.secureserver.net",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "no-reply@cmasealand.com",
+      pass: "Cma@2024cma"
+    }
+  });
+
+  try {
+    await transporter.sendMail({
+      from: '"CMA SEA LAND" <no-reply@cmasealand.com>',
+      to,
+      subject,
+      html
+    });
+
+    res.status(200).send("Email sent successfully");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Failed to send email");
+  }
+};
